a6237d96ab5df776ed0ffbe7db2d14ac7ba9e7dd
ierceg <ivan@softwaremarbles.com>
2013-12-20 19:04:56 -0300

    added api stub

--- public/index.php
+++ public/index.php
@@ -5,16 +5,71 @@ ini_set("include_path", ".:../:../epiphany/src/");
 include 'Epi.php';
 
 Epi::init('route');
-getRoute()->get('/', 'home');
-getRoute()->get('/contact', 'contactUs');
+getRoute()->get('/', 'heartbeat');
+getRoute()->get('/registerApp', 'registerApp');
+getRoute()->get('/login', 'login');
+getRoute()->get('/send', 'send');
+getRoute()->get('/getStatus', 'getStatus');
+getRoute()->get('/getStatistics', 'getStatistics');
+getRoute()->get('/logout', 'logout');
+getRoute()->get('/unregisterApp', 'unregisterApp');
 getRoute()->run();
 
-function home() {
-    echo 'You are at the home page';
+function heartbeat() {
+    $data = array(
+        'format' => 'json',
+        'status' => 'live'
+        );
+    echo json_encode($data);
 }
 
-function contactUs() {
-    echo 'Send us an email at <a href="mailto:ivan@softwaremarbles.com">ivan@softwaremarbles.com</a>';
+function registerApp() {
+    $data = array(
+        'appId' => 'appId',
+        'secret' => 'secret');
+    echo json_encode($data);
+}
+
+function login() {
+    $data = array(
+        'token' => 'token');
+    echo json_encode($data);
+}
+
+function send() {
+    $data = array(
+        'messageId' => 'messageId',
+        'status' => 'OK');
+    echo json_encode($data);
+}
+
+function getStatus() {
+    $data = array(
+        'messageId' => 'messageId',
+        'status' => 'status');
+    echo json_encode($data);
+}
+
+function getStatistics() {
+    $data = array(
+        'loginCount' => 'loginCount',
+        'sendCount' => 'sendCount',
+        'getStatusCount' => 'getStatus',
+        'getStatisticsCount' => 'getStatisticsCount',
+        'totalCount' => 'totalCount');
+    echo json_encode($data);
+}
+
+function logout() {
+    $data = array(
+        'status' => 'OK');
+    echo json_encode($data);
+}
+
+function unregisterApp() {
+    $data = array(
+        'status' => 'OK');
+    echo json_encode($data);
 }
 
 ?>
