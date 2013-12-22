8376012a96e7b1ce97711a26f71da8094dbf717a
ierceg <ivan@softwaremarbles.com>
2013-12-22 10:10:22 -0300

    Added fatal error handling

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php
 
+/**
+ * Handling fatal error
+ *
+ * @return void
+ */
+function fatalErrorHandler()
+{
+    # Getting last error
+    $error = error_get_last();
+
+    # Checking if last error is a fatal error
+    if(($error['type'] === E_ERROR) || ($error['type'] === E_USER_ERROR))
+    {
+        reportFailure('A fatal error has occurred in ' . $error['file'] . ', line ' . $error['line'] . ': ' . $error['message']);
+    }
+}
+
+# Registering shutdown function
+register_shutdown_function('fatalErrorHandler');
+
 //  Setup the include path before trying to include any other source files.
 ini_set("include_path", ".:../:../epiphany/src/");
 
